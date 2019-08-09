8edeba340bd7ac6627a907ba214adf4f31738234
yuuki.doi <dogrunruns@yahoo.co.jp>
2019-08-09 12:20:09 +0000

    user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  
+  root to: 'tasks#index'  
+ get 'signup', to: 'users#new'
+  #post 'signup', to: 'users#create'
+  #get 'users/index'
+  #get 'users/show'
+  #get 'users/new'
+  #post 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: 'tasks#index'
+
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_221358) do
+ActiveRecord::Schema.define(version: 2019_08_07_031049) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
